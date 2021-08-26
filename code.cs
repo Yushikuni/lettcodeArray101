c0976265e8e53bc6a0568f83371639681b74b7af
Kvetuse Husakova <42646031+Yushikuni@users.noreply.github.com>
2021-08-26 18:00:47 +0200

    add code
    just add some code

---
+++ code.cs
@@ -0,0 +1,24 @@
+public class Solution 
+{
+    public int FindMaxConsecutiveOnes(int[] nums) 
+    {
+        int same = 0;
+        for(int i = 0; i < nums.Length; i++)
+        {
+           int n = nums[i];
+            for(int j = 0; j < i; j++)
+            {
+                int o = nums[j];
+                if(n == o)
+                {
+                    ++same;
+                }
+                else
+                {
+                    same = 0;
+                }
+            }
+        }
+        return same;
+    }
+}
