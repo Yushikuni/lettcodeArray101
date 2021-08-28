da80d34f3631ff3db9e651eefda64262ba71b62d
Kvetuse Husakova <42646031+Yushikuni@users.noreply.github.com>
2021-08-28 07:56:36 +0200

    Update DuplicateZeros.cs

--- DuplicateZeros.cs
+++ DuplicateZeros.cs
@@ -2,19 +2,11 @@ public class Solution
 {
     public void DuplicateZeros(int[] arr) 
     {
-        //nové pole o velikosti 20
-        int[] pole = new int[20];
-        //kopie pole
         for(int i = 0; i < arr.Length; ++i)
         {
-            pole[i] = arr[i];
-        }
-        //uprava pole
-        for(int i = 0; i< pole.Length; ++i)
-        {
-            if(pole[i] == 0)
+            if(arr[i] == 0)
             {
-                pole[i+1] = pole[i];
+                arr[i+1] = arr[i];
             }
         }
     }
