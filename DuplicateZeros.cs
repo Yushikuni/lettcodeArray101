1b66b88c261e39b31197b1080b68f3081dda246b
Kvetuse Husakova <42646031+Yushikuni@users.noreply.github.com>
2021-08-28 08:46:05 +0200

    Update DuplicateZeros.cs

--- DuplicateZeros.cs
+++ DuplicateZeros.cs
@@ -6,7 +6,7 @@ public class Solution
         int n = arr.Length;
         //pocet duplicit
         int duplicity = 0;
-        //hlavni smycka
+        //zjisteni pocet duplicit
         for(int i = 0; i < n; ++i)
         {
             if(arr[i] == 0)
@@ -15,19 +15,25 @@ public class Solution
             }
         }
         
-        //kopie
-        for(int i = n; i >= 0; --i)
+        //uprava pole
+        for(int i = n - 1; i >= 0; --i)
         {
-            if(arr[i] == 0)
-            {
-
-                //Insert(arr[i+duplicity],0);
-                //arr[i + duplicity] = 0;
-                duplicity--;
-            }
-            else
+            //pokud je index vetsi nez n tak zmensi index
+            if(i + duplicity < n)
             {
                 arr[i + duplicity] = arr[i];
+            }
+            //pokud je tam nula tak proved duplicity
+            if(arr[i] == 0)
+            {
+                arr[i + duplicity] = 0;
+                //odecet duplicity
+                duplicity--; 
+                //kontrola kvuli preteceni ale asi nefunguje
+                if(i + duplicity < n)
+                {
+                    arr[i + duplicity] = arr[i];
+                }
             }
         }
     }
