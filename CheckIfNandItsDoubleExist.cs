54d3f4b59df2931d5f17b7257c2d38cbe3f572c5
Kvetuse Husakova <42646031+Yushikuni@users.noreply.github.com>
2021-09-04 08:04:15 +0200

    Create CheckIfNandItsDoubleExist.cs

---
+++ CheckIfNandItsDoubleExist.cs
@@ -0,0 +1,21 @@
+public class Solution 
+{
+    public bool CheckIfExist(int[] arr) 
+    {
+        if (arr == null || arr.Length == 0) 
+        {
+             return false;
+        }
+        for (int i = 0; i < arr.Length; ++i) 
+        {
+            for(int j = 0; j < i; ++j)
+            {
+                if(arr[i] == 2*arr[i] || arr[j] % 2 == 0)
+                {
+                    return true;
+                }
+            }
+        }        
+        return false;
+    }
+}
