f48030dd2001aa663506e39f3e0044ef43e620b9
Kvetuse Husakova <42646031+Yushikuni@users.noreply.github.com>
2021-09-05 15:39:54 +0200

    Update ReplaceElementswithGreatestElementonRightSide.cs

--- ReplaceElementswithGreatestElementonRightSide.cs
+++ ReplaceElementswithGreatestElementonRightSide.cs
@@ -1,1 +1,16 @@
-
+public class Solution 
+{
+    public int[] ReplaceElements(int[] arr) 
+    {
+        if (array == null) 
+        {
+            return null;
+        }
+        
+        for (int i = 0; i < arr.Length; ++i)
+        {
+                
+        }
+        
+    }
+}
