a584a5d46e5b8ce4d6bf3d32f4d2c44286e3fd25
Kvetuse Husakova <42646031+Yushikuni@users.noreply.github.com>
2021-09-24 18:31:58 +0200

    Update ReplaceElementswithGreatestElementonRightSide.cs

--- ReplaceElementswithGreatestElementonRightSide.cs
+++ ReplaceElementswithGreatestElementonRightSide.cs
@@ -2,15 +2,26 @@ public class Solution
 {
     public int[] ReplaceElements(int[] arr) 
     {
-        if (array == null) 
+        int velikost = arr.Length;
+        
+        int maxPrava = arr[velikost-1];
+        
+        if (arr == null || velikost == 0) 
         {
             return null;
         }
         
-        for (int i = 0; i < arr.Length; ++i)
+        for (int i = velikost - 2; i >= 0; i--)
         {
-                
+            int prvek = arr[i];
+            arr[i] = maxPrava;
+            if(maxPrava < prvek)
+            {
+                maxPrava = prvek;
+            }
         }
+        arr[velikost-1]=-1;
+        
      return arr;   
     }
 }
